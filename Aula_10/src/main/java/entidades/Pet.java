46a66c4a5a4e2acda99cd1d156dbaea5117f0cb8
Thiago Coutinho <2022211510098@iesp.edu.br>
2023-05-23 21:18:10 -0300

    Projeto

--- Aula_10/src/main/java/entidades/Pet.java
+++ Aula_10/src/main/java/entidades/Pet.java
@@ -1,6 +1,15 @@
 package entidades;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String nome;
     private int anoNascimento;
     private double peso;
